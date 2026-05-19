9f0975701ee695159f26e8eb9a21bc5153ab9db8
sreeb29 <sree.bhattar@gmail.com>
2026-05-19 01:16:27 +0000

    Added basic_cleaning steps to remove the ouliers in the sample.csv.

--- main.py
+++ main.py
@@ -50,8 +50,19 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
+            #Implementing EDA process  to clean the sample.csv#
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "input_artifact": "sample.csv:lates",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outlies removed",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                }
+            )
             ##################
             pass
 
